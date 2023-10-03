a7d05ce35acc98fc55ffb341764944d567955a0c
Ali <ali.hussein.pre@gmail.com>
2023-10-03 18:34:00 +0200

    feat: tweak name page UI

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -11,9 +11,10 @@ export default async function NamePage({ params }: { params: { id: string } }) {
   const pageRes = await getNamePage(params.id);
   const page = pageRes[0]!;
   return (
-    <section className="mx-auto w-full max-w-3xl grow rounded-sm bg-zinc-100 px-3 pb-8 pt-12 shadow-xl md:px-8">
-      <div className="mb-4 border-b border-zinc-200 pb-1 flex-row-between">
-        <h2 className="mb-4 text-3xl font-bold text-zinc-700">{page?.title}</h2>
+    <section className="mx-auto w-full max-w-3xl grow rounded-sm bg-zinc-100 px-3 pb-8 pt-12 shadow-xl md:px-8 flex-col-center">
+
+      <div className="mb-4 border-b border-zinc-200 pb-1 flex-row-between w-full">
+        <h2 className="text-3xl font-bold text-zinc-700">{page?.title}</h2>
         <Link
           href="/"
           className="center h-10 w-10 rounded-xl bg-zinc-200 duration-200 active:translate-y-1"
@@ -21,10 +22,11 @@ export default async function NamePage({ params }: { params: { id: string } }) {
           <FaArrowLeft className="text-zinc-600" />
         </Link>
       </div>
-      <div className="prose prose-zinc w-full max-w-3xl border-b border-zinc-200 pb-3 pt-4 prose-h2:text-zinc-700 prose-p:text-lg prose-p:md:text-xl">
+
+      <div className="prose prose-zinc w-full max-w-3xl border-b border-zinc-200 pb-3 pt-4 prose-h2:text-zinc-700 prose-p:text-lg prose-p:md:text-xl grow">
         <PortableText value={page.content} />
       </div>
-      <p hidden={!page?.source} className="pt-2 italic text-zinc-500">
+      <p hidden={!page?.source} className="pt-2 italic text-zinc-500 w-full">
         {content.source}: {page?.source}
       </p>
     </section>
