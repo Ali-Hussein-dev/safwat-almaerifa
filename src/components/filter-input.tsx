c7e73c7877a33c4d10f232fcebf425b08636065c
Ali <ali.hussein.pre@gmail.com>
2023-10-03 20:23:38 +0200

    feat: add focus shortcut

--- src/components/filter-input.tsx
+++ src/components/filter-input.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { useHotkeys } from "@mantine/hooks";
 import * as React from "react";
 import { FaSearch } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
+
 const content = {
   placeholder: "ابحث",
 };
@@ -16,10 +18,16 @@ export function FilterInput({ input, setInput }: Props) {
     setInput(v);
     setValue(v);
   };
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
+  const setFocus = () => {
+    inputRef.current?.focus();
+  };
+  useHotkeys([["mod+K", () => setFocus()]]);
   return (
-    <div className="mb-4 w-full gap-1 rounded border border-zinc-200 bg-gradient-to-t from-zinc-50 to-transparent px-3 duration-300 flex-row-start focus-within:border-zinc-300 focus-within:shadow-xl">
+    <div className="mb-2 w-full gap-1 rounded border border-zinc-200 bg-gradient-to-t from-zinc-50 to-transparent px-3 duration-300 flex-row-start focus-within:border-zinc-300 focus-within:shadow-xl">
       <FaSearch className="text-zinc-300" />
       <input
+        ref={inputRef}
         value={value}
         onChange={(e) => {
           onChange(e.target.value);
@@ -27,7 +35,7 @@ export function FilterInput({ input, setInput }: Props) {
         placeholder={content.placeholder}
         className="h-12 w-full grow rounded-lg bg-transparent px-4 focus:outline-none"
       />
-      {value && (
+      {value ? (
         <button
           type="button"
           onClick={() => onChange("")}
@@ -35,6 +43,10 @@ export function FilterInput({ input, setInput }: Props) {
         >
           <IoClose className="text-zinc-500" size="20" />
         </button>
+      ) : (
+        <span className="text-zinc-500">
+          {navigator.userAgent.includes("Mac") ? "cmd+K" : "ctrl+K"}
+        </span>
       )}
     </div>
   );
