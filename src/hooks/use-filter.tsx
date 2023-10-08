29125157449072da387e173dfe1df1773dede7be
Ali <ali.hussein.pre@gmail.com>
2023-10-08 10:53:04 +0200

    refactor: run prettier

--- src/hooks/use-filter.tsx
+++ src/hooks/use-filter.tsx
@@ -2,15 +2,21 @@
 import { type NamePage } from "@/types/name-project";
 import * as React from "react";
 
-export const useFilter = ({ list = [] }: { list: Omit<NamePage,"content">[] }) => {
+export const useFilter = ({
+  list = [],
+}: {
+  list: Omit<NamePage, "content">[];
+}) => {
   const [input, setInput] = React.useState("");
   const [filtered, setFiltered] = React.useState(list);
   React.useEffect(() => {
     if (Array.isArray(list)) {
-      const trimedInput= input.trim()
-      const filList = list.filter((item) => item.key.includes(trimedInput));
+      const trimedInput = input.trim();
+      const filterByInput = (item: Omit<NamePage, "content">) =>
+        item.key.includes(trimedInput);
+      const filteredList = list.filter(filterByInput);
       console.log("filtering...", input);
-      setFiltered(filList);
+      setFiltered(filteredList);
     }
   }, [input, list]);
 
