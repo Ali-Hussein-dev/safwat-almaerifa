7b3d590b35c4c59f8cb83e68108cfcfb23e754ac
Ali <ali.hussein.pre@gmail.com>
2023-10-24 22:23:13 +0200

    feat: re-organise the pages

---
+++ src/app/names-of-Allah/page.tsx
@@ -0,0 +1,29 @@
+import { NamesList } from "@/components/names-list";
+import { getAllPages } from "../../../sanity/lib/get-name-page";
+
+const content = {
+  h1: "أسماء الله الحسنى",
+  p: "وَلِلَّهِ الْأَسْمَاءُ الْحُسْنَىٰ فَادْعُوهُ بِهَا ۖ وَذَرُوا الَّذِينَ يُلْحِدُونَ فِي أَسْمَائِهِ ۚ سَيُجْزَوْنَ مَا كَانُوا يَعْمَلُونَ",
+};
+export const metadata = {
+  title: "أسماء الله الحسنى",
+  description: "أسمى الله الحسنى مع شرح لكل اسم",
+};
+
+const NamesOfAllah = async () => {
+  const pages = (await getAllPages()).sort((a, b) => +a.slug - +b.slug);
+
+  return (
+    <div className="mx-auto max-w-3xl space-y-8 pt-4 md:pt-20">
+      <div>
+        <h1 className="mb-1 text-center text-lg font-extrabold text-zinc-700 sm:text-2xl md:text-4xl">
+          {content.h1}
+        </h1>
+        <p className="text-center text-lg text-zinc-600">{content.p}</p>
+      </div>
+      <NamesList names={pages} />
+    </div>
+  );
+};
+
+export default NamesOfAllah;
