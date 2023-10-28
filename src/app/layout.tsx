86b61d547963d5351e5283f8db28b8b6a2e14797
Ali <ali.hussein.pre@gmail.com>
2023-10-28 13:01:09 +0200

    feat: add route-change loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "../styles/globals.css";
 import * as React from "react";
 import Link from "next/link";
 import { HiHome } from "react-icons/hi";
+import NextTopLoader from "nextjs-toploader";
+
 const fontFamily = Tajawal({
   subsets: ["arabic"],
   display: "swap",
@@ -12,7 +14,7 @@ const fontFamily = Tajawal({
 const Footer = () => {
   return (
     <div
-      className="flex-row-center sticky bottom-0 -z-10 mx-auto h-14 w-full border-t border-zinc-300 bg-zinc-700 px-2 py-2 text-zinc-100"
+      className="sticky bottom-0 -z-10 mx-auto h-14 w-full border-t border-zinc-300 bg-zinc-700 px-2 py-2 text-zinc-100 flex-row-center"
       dir="ltr"
     >
       <div className="max-w-3xl">
@@ -33,9 +35,10 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl" className={fontFamily.className}>
       <body className="scroll-smooth antialiased">
+        <NextTopLoader color="#84cc16" showSpinner={false} speed={300} />
         <main className="z-10 mx-auto flex min-h-screen flex-col justify-between gap-4 bg-zinc-50 shadow-lg">
           <header className="h-12 w-full bg-zinc-100 shadow-lg ">
-            <div className="flex-row-start mx-auto h-12 w-full max-w-3xl px-2 font-semibold text-zinc-700">
+            <div className="mx-auto h-12 w-full max-w-3xl px-2 font-semibold text-zinc-700 flex-row-start">
               <Link href="/">
                 <HiHome size="23" />
               </Link>
