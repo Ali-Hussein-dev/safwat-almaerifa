1e1dcc375c5ad57f74cd75638d67d777a2a26db5
Ali <ali.hussein.pre@gmail.com>
2023-10-30 09:06:09 +0100

    feat: improve SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { Hero } from "@/components/Hero/Hero";
-
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "مواضيع إسلامية أساسية",
+  description: "المواضيع الإسلامية التي يحتاجها كل مسلم",
+  category: "إسلام",
+};
 export default function RootPage() {
   return (
     <div className="relative h-full w-full grow px-2 sm:px-4">
--- src/app/quran-topics/page.tsx
+++ src/app/quran-topics/page.tsx
@@ -1,22 +1,27 @@
 import { getQuranTopicsTitles } from "../../../sanity/lib/get-quran-topics";
 import * as React from "react";
 import { QuranTopics } from "@/components/quran-topics";
-
+import { H1 } from "@/components/headers";
+import type { Metadata } from "next";
 const content = {
   h1: "مواضيع سور القرآن الكريم",
   description: "شرح مختصر لمواضيع السور التي تساعد على الفهم والتدبر",
   loading: "جاري التحميل...",
 };
 
+export const metadata: Metadata = {
+  title: content.h1,
+  description: content.description,
+  category: "قرآن",
+};
+export const revalidate = 1000;
 const QuranTopicsPage = async () => {
   const res = await getQuranTopicsTitles();
   return (
     <div className="grow">
-      <div className="mx-auto max-w-3xl pb-8 pt-4 text-zinc-700 md:pt-12">
+      <div className="mx-auto max-w-3xl pb-8 pt-4 text-zinc-700 md:pt-20">
         <div className="mb-8 ">
-          <h1 className="mb-1 text-center text-2xl font-bold text-lime-600">
-            {content.h1}
-          </h1>
+          <H1 value={content.h1} />
           <p className="text-center">{content.description}</p>
         </div>
         <React.Suspense
