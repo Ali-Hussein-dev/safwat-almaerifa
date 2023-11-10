36f5bb0501251f131cbe7ecc2eefd6512c08025f
Ali <ali.hussein.pre@gmail.com>
2023-11-10 23:31:07 +0100

    fix: add card bg-color

--- src/components/God-Names/tab-content-similar-names.tsx
+++ src/components/God-Names/tab-content-similar-names.tsx
@@ -8,7 +8,7 @@ export const TabContentSimilarNames = async () => {
   const list = await getSimilarNames();
   return (
     <TabsContent value="similars" className="pt-5">
-      <div className="w-full gap-8 flex-col-center">
+      <div className="w-full gap-8 bg-zinc-50 flex-col-center">
         {list.map((o, i) => (
           <div
             className="rounded-xl border-[0.5px] border-b-2 border-zinc-300 px-4 py-8 shadow-lg"
@@ -16,7 +16,7 @@ export const TabContentSimilarNames = async () => {
           >
             <div className="relative mb-4 gap-1 flex-row-center">
               <div className="absolute h-[1.5px] w-full bg-gradient-to-r from-transparent via-lime-300 to-transparent" />
-              <h2 className="center z-[2] max-w-fit  grow bg-zinc-50 px-4 font-bold text-lime-500">
+              <h2 className="center z-[2] max-w-fit grow bg-zinc-50 px-4 font-bold text-lime-500">
                 {o.title}
               </h2>
             </div>
