c101c27af036a03561a9d476573f48af13fdc4e8
Ali <ali.hussein.pre@gmail.com>
2023-11-17 00:47:26 +0100

    feat: add scroll-to button to pages

--- src/components/reading-container/reading-page.tsx
+++ src/components/reading-container/reading-page.tsx
@@ -5,6 +5,7 @@ import { CiDark, CiLight } from "react-icons/ci";
 import { BackButton } from "../back-button";
 import { ActionIcon } from "../action-icon";
 import { useReadingPreferences } from "@/hooks/use-reading-preferences";
+import { ScrollTo } from "../scroll-to";
 
 const classes = {
   dark: "bg-zinc-800 text-zinc-300",
@@ -31,42 +32,45 @@ export const ReadingPage = ({
   const { theme, fontSize, incFontSize, decFontSize, setTheme } =
     useReadingPreferences();
   return (
-    <div
-      className={clsx(
-        "flex flex-col gap-1",
-        "shadow-side mx-auto h-full w-full max-w-[755px] grow px-3 pb-6 shadow-xl duration-150 sm:rounded md:px-8",
-        classes[theme],
-      )}
-    >
-      <div className="w-full py-2 flex-row-between">
-        <div className="gap-2 flex-row-start">
-          <ActionIcon onClick={() => setTheme()}>
-            {theme === "light" ? <CiDark size="22" /> : <CiLight size="22" />}
-          </ActionIcon>
-          <ActionIcon
-            disabled={fontSize === classes.textSize.length - 1}
-            onClick={() => incFontSize()}
-          >
-            +A
-          </ActionIcon>
-          <ActionIcon disabled={fontSize === 0} onClick={() => decFontSize()}>
-            -A
-          </ActionIcon>
-        </div>
-        <BackButton />
-      </div>
-      {top}
+    <>
       <div
         className={clsx(
-          "prose prose-zinc max-w-full prose-p:leading-10",
-          theme === "dark" && "prose-invert",
-          classes.textSize[fontSize],
-          grow && "grow",
+          "flex flex-col gap-1",
+          "shadow-side mx-auto h-full w-full max-w-[755px] grow px-3 pb-6 shadow-xl duration-150 sm:rounded md:px-8",
+          classes[theme],
         )}
       >
-        {body}
+        <div className="w-full py-2 flex-row-between">
+          <div className="gap-2 flex-row-start">
+            <ActionIcon onClick={() => setTheme()}>
+              {theme === "light" ? <CiDark size="22" /> : <CiLight size="22" />}
+            </ActionIcon>
+            <ActionIcon
+              disabled={fontSize === classes.textSize.length - 1}
+              onClick={() => incFontSize()}
+            >
+              +A
+            </ActionIcon>
+            <ActionIcon disabled={fontSize === 0} onClick={() => decFontSize()}>
+              -A
+            </ActionIcon>
+          </div>
+          <BackButton />
+        </div>
+        {top}
+        <div
+          className={clsx(
+            "prose prose-zinc max-w-full prose-p:leading-10",
+            theme === "dark" && "prose-invert",
+            classes.textSize[fontSize],
+            grow && "grow",
+          )}
+        >
+          {body}
+        </div>
+        {bottom}
       </div>
-      {bottom}
-    </div>
+      <ScrollTo />
+    </>
   );
 };
