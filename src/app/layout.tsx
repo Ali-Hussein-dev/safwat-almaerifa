ef41845c613547e10622b4bfa429cfc8d8cfff37
Ali <ali.hussein.pre@gmail.com>
2023-11-25 01:08:53 +0100

    feat: add font & logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,45 @@
-import { Tajawal } from "next/font/google";
+import { Tajawal, El_Messiri } from "next/font/google";
 import "../styles/globals.css";
 import * as React from "react";
 import Link from "next/link";
-import { HiHome } from "react-icons/hi";
 import NextTopLoader from "nextjs-toploader";
 import { Footer } from "@/components/Footer";
+import Image from "next/image";
 
-const fontFamily = Tajawal({
+// use for headings
+const tajawal = Tajawal({
   subsets: ["arabic"],
   display: "auto",
   weight: ["200", "300", "400", "500", "700", "800", "900"],
+  variable: "--font-tajawal",
 });
 
-// const Links = [{ name: "الرئيسية", href: "/" }];
+// use for body
+const elMessiri = El_Messiri({
+  subsets: ["arabic"],
+  display: "auto",
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-elMessiri",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ar" dir="rtl" className={fontFamily.className}>
+    <html
+      lang="ar"
+      dir="rtl"
+      className={`${tajawal.variable} ${elMessiri.variable}`}
+    >
       <body className="relative scroll-smooth antialiased">
         <NextTopLoader color="#84cc16" showSpinner={false} speed={300} />
         <main className="mx-auto flex min-h-screen flex-col justify-between gap-4 bg-zinc-50/30 shadow-lg">
           <header className="h-12 w-full bg-zinc-100 shadow-lg ">
             <div className="mx-auto h-12 w-full max-w-3xl px-2 font-semibold text-zinc-700 flex-row-start">
               <Link href="/">
-                <HiHome size="23" />
+                <Image src="/logo.svg" width="130" height="18" alt="logo" />
               </Link>
             </div>
           </header>
